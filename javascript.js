3c21edf1620e1ba7bcc3eb71ae5deef35c570e83
Ammanuel <wondwossenammanuel@gmail.com>
2022-06-22 19:29:53 -0500

    Completed Assignmet     -Created a new funtion called score that acts as a helper function.     -Fixed score keeping bug.     -Made the prompt case insensitive in order to allow capitalization.     -Made the program loop 5 times before outputting the winner.
        Bugs:
        -Socre function works but I couldn't figure out why my score incremented
         by 2 every time.

--- javascript.js
+++ javascript.js
@@ -1,35 +1,65 @@
+let playerSelection = null;
+let computerSelection = null;
+let playerScore = 0;
+let computerScore = 0;
+let testVar = 0;
+
 function computerPlay(){
     var index = Math.floor(Math.random() * 3);
-    let state = ["Rock", "Paper", "Scissor"];
-    console.log(index);
+    let state = ["rock", "paper", "scissor"];
     return state[index]; 
 }
 
-function play(a, b){
-    if(playerSelection === "Paper" && computerSelection === "Rock"){
-        console.log("You win! Paper beats rock.")
-    } else if(playerSelection === "Rock" && computerSelection === " Scissor"){
+function playRound(a, b){
+    if(playerSelection === "paper" && computerSelection === "rock"){
+        ++playerScore;
+        return("You win! Paper beats rock.")
+    } else if(playerSelection === "rock" && computerSelection === " scissor"){
+        ++playerScore;
         return("You win! Rock beats scissor.");
-    } else if(playerSelection === "Scissor" && computerSelection === "Paper"){
+    } else if(playerSelection === "scissor" && computerSelection === "paper"){
+        ++playerScore;
         return("You win! Scissor beats paper.");
-    } else if(playerSelection === "Rock" && computerSelection === "Paper"){
+    } else if(playerSelection === "rock" && computerSelection === "paper"){
+        ++computerScore;
         return("You lose! Paper beats rock.")
-    } else if(playerSelection === "Paper" && computerSelection === "Scissor"){
+    } else if(playerSelection === "paper" && computerSelection === "scissor"){
+        ++computerScore;
         return("You lose! Scissor beats paper");
-    } else if(playerSelection === "Scissor" && computerSelection === "Rock"){
+    } else if(playerSelection === "scissor" && computerSelection === "rock"){
+        ++computerScore;
         return("You lose! Rock beats Scissor");
-
-    } else if(playerSelection === "Rock" && computerSelection === "Rock"){
+    } else if(playerSelection === "rock" && computerSelection === "rock"){
         return("Its a tie!");
-    } else if(playerSelection === "Paper" && computerSelection === "Paper"){
+    } else if(playerSelection === "paper" && computerSelection === "paper"){
         return("Its a tie!");
-    } else if(playerSelection === "Scissor" && computerSelection === "Scissor"){
+    } else if(playerSelection === "scissor" && computerSelection === "scissor"){
         return("Its a tie!");
     } 
 }
 
-const playerSelection = "Paper";
-const computerSelection = computerPlay();
-console.log(play(playerSelection, computerSelection));
 
-computerPlay();
+function score(c, d){
+    if(playerScore > computerScore){
+        return("Congratulations! You are the winner");
+    } else if(playerScore < computerScore){
+        return("Game Over. Better luck next time.");
+    } else if(playerScore === computerScore){
+        return("No winner! It's a tie game.");
+    }
+}
+score(playerScore, computerScore);
+
+function game(){
+    for(let i = 0; i < 5; i++){
+        playerSelection = prompt("Enter your choice: ").toLowerCase();
+        computerSelection = computerPlay();
+        playRound();
+        console.log(playRound());
+    }
+    console.log(score());
+
+}
+
+playRound(playerSelection, computerSelection);
+game();
